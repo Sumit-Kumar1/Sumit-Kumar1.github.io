00e74fea09951f7fec0adee9da2672ffe531b45c
Sumit Kumar <kumarsumitjat298@gmail.com>
2022-09-12 18:18:38 +0530

    Upgrade package and  new Sidebar for profile icons

---
+++ components/ProfileSideBar.js
@@ -0,0 +1,25 @@
+import {SiCodechef, SiKaggle, SiLeetcode} from "react-icons/si";
+import Link from "next/link";
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+
+export default function ProfileSideBar(){
+    return (
+        <ul className={"fixed z-10 flex flex-col hover:cursor-pointer left-[5%] p-5 h-full justify-center gap-10 border-2"}>
+            <li>
+                <Link href="https://www.kaggle.com/sumit1kumar"><a target={"_blank"}><SiKaggle size={32}></SiKaggle></a></Link>
+            </li>
+            <li>
+                <Link href="https://github.com/Sumit-Kumar1"><a target={"_blank"}><FaGithub size={25}></FaGithub></a></Link>
+            </li>
+            <li >
+                <Link href="https://www.linkedin.com/in/sumit1kumar/"><a target={"_blank"}><FaLinkedin size={25}></FaLinkedin></a></Link>
+            </li>
+            <li>
+                <Link href={"https://leetcode.com/Dark_Blood/"}><a target={"_blank"}><SiLeetcode size={25}></SiLeetcode></a></Link>
+            </li>
+            <li>
+                <Link href={"https://www.codechef.com/users/dark_blood"}><a target={"_blank"}><SiCodechef size={25}></SiCodechef></a></Link>
+            </li>
+        </ul>
+    );
+}
--- components/navbar.js
+++ components/navbar.js
@@ -14,28 +14,7 @@ function Navbar(){
                 <li>Projects</li>
                 <li>Contact Me</li>
             </ul>
-            <ul className={"flex gap-4 pt-2 hover:cursor-pointer"}>
-                <li>
-                    <SiKaggle size={32}></SiKaggle>
-                    <Link href="https://www.kaggle.com/sumit1kumar"><a></a></Link>
-                </li>
-                <li>
-                    <FaGithub size={25}></FaGithub>
-                    <Link href="https://github.com/Sumit-Kumar1"><a></a></Link>
-                </li>
-                <li >
-                    <FaLinkedin size={25}></FaLinkedin>
-                    <Link href="https://www.linkedin.com/in/sumit1kumar/"><a></a></Link>
-                </li>
-                <li>
-                    <SiLeetcode size={25}></SiLeetcode>
-                    <Link href={"https://leetcode.com/Dark_Blood/"}><a></a></Link>
-                </li>
-                <li>
-                    <SiCodechef size={25}></SiCodechef>
-                    <Link href={"https://www.codechef.com/users/dark_blood"}><a></a></Link>
-                </li>
-            </ul>
+
         </nav>
     )
 }
