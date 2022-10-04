a45a10c1c4f8c0c7e54224b3e07801a67266a0b4
Sumit Kumar <kumarsumitjat298@gmail.com>
2022-10-04 16:36:19 +0530

    Make Nav bar component render using UseState Hook

--- components/contactMe.js
+++ components/contactMe.js
@@ -16,8 +16,8 @@ export default function ContactMe() {
                         Ward No. 04, Behind Agarwal Petrol Pump, Neem Ka Thana, Sikar,
                         Rajasthan, 332713
                     </address>
-                    <p>kumarsumitjat298@gmail.com</p>
-                    <p>+91-9828-439864, +91-6350-104794</p>
+                    <p className="text-green-500">kumarsumitjat298@gmail.com</p>
+                    <p>+91-9828-439864 <br/> +91-6350-104794</p>
                 </div>
                 <form className={"flex flex-col gap-y-10 w-3/4"}>
                     <input
@@ -45,7 +45,7 @@ export default function ContactMe() {
                     />
                     <button
                         className={
-                            "xs:w-full md:w-1/4 xl:w-1/3 border-2 rounded-2xl p-3 dark:hover:bg-[#1d1d1d] hover:bg-green-400 mx-auto hover:font-bold hover:cursor-pointer"
+                            "xs:w-full md:w-1/4 xl:w-1/3 border-2 rounded-2xl p-3 dark:hover:bg-[#0d0d0d] hover:bg-green-400 mx-auto hover:font-bold hover:cursor-pointer"
                         }
                     >
                         Send Now
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Navbar from '../components/navbar';
 import ProfileSideBar from "../components/ProfileSideBar";
@@ -5,32 +6,30 @@ import ContactMe from "../components/contactMe";
 import Skill from "../components/skills";
 import HomeMain from '../components/home';
 import Projects from "../components/Projects";
-import Link from "next/link";
 
 export default function Home() {
-  let clicks = 0;
-  // const [click, setclick] = useState(0)
+  const [click, setClick] = useState(0)
   return (
     <div className={"h-screen dark:bg-[#282828] bg-white dark:text-white justify-center align-middle justify-items-center"}>
       <Head>
         <title>My Portfolio</title>
         <meta name="description" content="My Resume website created using Next JS and Tailwind CSS" />
-      </Head>      
+      </Head>
       <nav>
         <ul className={"flex gap-x-5 text-2xl font-light font-sans hover:cursor-pointer justify-center pt-10"}>
-          <li className="hover:underline-offset-8 hover:underline hover:decoration-wavy hover:decoration-green-400 hover:font-semibold hover:decoration-1"><Link href={"./"}><a>Home</a></Link></li>
-          <li className="hover:underline-offset-8 hover:underline hover:decoration-wavy hover:decoration-green-400 hover:font-semibold hover:decoration-1">Skills</li>
-          <li className="hover:underline-offset-8 hover:underline hover:decoration-wavy hover:decoration-green-400 hover:font-semibold hover:decoration-1">Projects</li>
-          <li className="hover:underline-offset-8 hover:underline hover:decoration-wavy hover:decoration-green-400 hover:font-semibold hover:decoration-1">Contact Me</li>
+          <li><button onClick={() => setClick(0)}>Home</button></li>
+          <li><button onClick={() => setClick(1)}>Skills</button></li>
+          <li><button onClick={() => setClick(2)}>Projects</button></li>
+          <li><button onClick={() => setClick(3)}>Contact Me</button></li>
         </ul>
       </nav>
       <main className={"box-content justify-items-center m-24 py-6 px-24 dark:text-[#e8e8e8]"}>
         <ProfileSideBar />
-        <p className={"fixed bottom-[47.2%] -rotate-90 z-1 -right-20 justify-items-center"}>@Copyrights to Sumit Kumar</p>
-        <HomeMain />
-        {/* <Skill /> */}
-        {/* <ContactMe /> */}
-        {/* <Projects/> */}
+        <p className={"fixed bottom-[47.2%] -rotate-90 z-1 -right-20 justify-items-center"}>@Copyrights to Sumit Kumar (2022)</p>
+        {/* Logically rendering component based on state of click count */}
+        {
+          (click == 0 ? <HomeMain></HomeMain> : (click == 1 ? <Skill></Skill> : (click == 2 ? <Projects></Projects>: (click == 3 ? <ContactMe></ContactMe> : <HomeMain></HomeMain>))))
+        }
       </main>
     </div>
   )
