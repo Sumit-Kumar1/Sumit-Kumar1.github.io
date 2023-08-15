3694fc7e65343f21b9caf1068bb5187a76b3ffc0
Sumit Kumar <kumarsumitjat298@gmail.com>
2023-08-15 07:37:49 +0530

    Added mobile screen config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: { }
+    extend: { },
+    screens: {
+      "mobile":'320px',
+      // => @media (min-width:320px) {....}
+    }
   },
   plugins: [],
 }
