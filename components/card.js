71bb166460e16afc92d9d43a77c537c0dc1a8046
Sumit Kumar <kumarsumitjat298@gmail.com>
2023-08-15 09:53:11 +0530

    refined cards

--- components/card.js
+++ components/card.js
@@ -1,10 +1,13 @@
-export default function Card(){
-    return (
-    
-        <div class="inline-block px-3">
-        <div
-          class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-        ></div>
+import Image from "next/image";
+
+export default function Card(props) {
+  return (
+    <div class="inline-block px-3">
+      <div class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out text-black text-justify grid p-5 grid-flow-row">
+        <div className="h-2/3">{props.icon}</div>
+        <h1 className="text-2xl font-bold">{props.Text}</h1>
+        <p>{props.Desc}</p>
       </div>
-    );
+    </div>
+  );
 }
--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,29 +1,22 @@
 import Card from "../../components/card"
+import {TbBrandCpp, TbBrandGolang, TbBrandCSharp, TbBrandPython, TbBrandMysql} from "react-icons/tb"
 
 export default function Skills() {
-    return (
-        <div class="flex flex-co m-auto p-auto">
-        <h1
-                class="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-center"
-              >
-                I am Skilled at ... 
-              </h1>
-              <div
-                class="flex overflow-x-scroll pb-10 hide-scroll-bar"
-              >
-                <div
-                  class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
-                >
-                 <Card/>
-                 <Card/>
-                 <Card/>
-                 <Card/>
-                 <Card/>
-                 <Card/>
-                 <Card/>
-                 <Card/>
-                </div>
-              </div>
-              </div>
-    );
+  return (
+    <div className="flex flex-col m-auto p-auto md:w-7/12">
+      <h1 className="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-2xl text-center">
+        Programming Languages
+      </h1>
+      {/* cards carousel*/}
+      <div class="flex overflow-x-scroll pb-10">
+        <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10">
+          <Card Text="GO" icon={<TbBrandGolang size={128}/>} Desc="Experienced at professional level"/>
+          <Card Text="CPP" Img={<TbBrandCpp size={64}/>} Desc="Experienced Project Level"/>
+          <Card Text="C#" Img={<TbBrandCSharp size={128}/>} Desc="Experienced Project Level"/>
+          <Card Text="Python" Img={<TbBrandPython size={128}/>} Desc="Experienced Project Level"/>
+          <Card Text="SQL" Img={<TbBrandMysql size={128}/>} Desc="Experienced Project Level"/>
+        </div>
+      </div>
+    </div>
+  );
 }
